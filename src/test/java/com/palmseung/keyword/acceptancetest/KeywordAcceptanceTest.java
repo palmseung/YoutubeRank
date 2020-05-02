484cc9b52e8271f1ad014e50da21205d56fd64a5
LeeSeungHee <palmseung@gmail.com>
2020-05-03 00:05:23 +0900

    Refactor : refactor codes

--- src/test/java/com/palmseung/keyword/acceptancetest/KeywordAcceptanceTest.java
+++ src/test/java/com/palmseung/keyword/acceptancetest/KeywordAcceptanceTest.java
@@ -7,22 +7,21 @@ import com.palmseung.member.dto.LoginResponseView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-import static com.palmseung.keyword.KeywordConstant.BASE_URI_KEYWORD_API;
 import static com.palmseung.member.MemberConstant.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class KeywordAcceptanceTest extends AbstractAcceptanceTest {
     private MemberHttpTest memberHttpTest;
+    private KeywordHttpTest keywordHttpTest;
     private String accessToken;
 
     @BeforeEach
     void setUp() {
         memberHttpTest = new MemberHttpTest(webTestClient);
+        keywordHttpTest = new KeywordHttpTest(webTestClient);
         memberHttpTest.createMember(TEST_EMAIL, TEST_NAME, TEST_PASSWORD);
         LoginResponseView responseBody = memberHttpTest.login(TEST_EMAIL, TEST_PASSWORD).getResponseBody();
         accessToken = responseBody.getAccessToken();
@@ -34,14 +33,7 @@ public class KeywordAcceptanceTest extends AbstractAcceptanceTest {
         //given
         String keyword = "queendom";
 
-        List<KeywordResponseView> responseViews = webTestClient.post().uri(BASE_URI_KEYWORD_API)
-                .header("Authorization", accessToken)
-                .accept(MediaType.APPLICATION_JSON)
-                .body(Mono.just(keyword), String.class)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBodyList(KeywordResponseView.class)
-                .returnResult().getResponseBody();
+        List<KeywordResponseView> responseViews = keywordHttpTest.addMyKeyword(keyword, accessToken);
 
         assertThat(responseViews.get(0).getId()).isEqualTo(1l);
         assertThat(responseViews.get(0).getKeyword()).isEqualTo("queendom");
