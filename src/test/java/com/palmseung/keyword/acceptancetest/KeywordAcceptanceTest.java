1eaf8290e3496d797f0c5c7e36ca9c5d1af2e507
LeeSeungHee <palmseung@gmail.com>
2020-05-03 02:22:50 +0900

    Test (keyword) : add AcceptanceTest for retrieving my all keywords
    * Add KeywordAcceptanceTest.retrieveAllMyKeywords

--- src/test/java/com/palmseung/keyword/acceptancetest/KeywordAcceptanceTest.java
+++ src/test/java/com/palmseung/keyword/acceptancetest/KeywordAcceptanceTest.java
@@ -7,9 +7,11 @@ import com.palmseung.member.dto.LoginResponseView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
 import java.util.List;
 
+import static com.palmseung.keyword.KeywordConstant.BASE_URI_KEYWORD_API;
 import static com.palmseung.member.MemberConstant.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,5 +41,24 @@ public class KeywordAcceptanceTest extends AbstractAcceptanceTest {
         //then
         assertThat(responseViews.get(0).getId()).isEqualTo(1l);
         assertThat(responseViews.get(0).getKeyword()).isEqualTo("queendom");
+    }
+
+    @DisplayName("My Keword 조회")
+    @Test
+    public void retrieveAllMyKeywords() {
+        //given
+        keywordHttpTest.addMyKeyword("queendom", accessToken);
+        keywordHttpTest.addMyKeyword("(g)idle", accessToken);
+
+        //when
+        List<KeywordResponseView> responseViews = webTestClient.get().uri(BASE_URI_KEYWORD_API)
+                .header("Authorization", accessToken)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(KeywordResponseView.class)
+                .returnResult().getResponseBody();
+
+        assertThat(responseViews).hasSize(2);
     }
 }
