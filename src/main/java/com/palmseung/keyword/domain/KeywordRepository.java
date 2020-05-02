7dab6fc80a3be80cdadb81ff31c8de56a375d94c
LeeSeungHee <palmseung@gmail.com>
2020-05-02 18:38:07 +0900

    Test (keyword) : add test for validating to create a keyword
    * Add KeywordServiceTest.createKeywordWhenExisting

--- src/main/java/com/palmseung/keyword/domain/KeywordRepository.java
+++ src/main/java/com/palmseung/keyword/domain/KeywordRepository.java
@@ -2,5 +2,8 @@ package com.palmseung.keyword.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+    Optional<Keyword> findByKeyword(String keyword);
 }
--- src/test/java/com/palmseung/keyword/service/KeywordServiceTest.java
+++ src/test/java/com/palmseung/keyword/service/KeywordServiceTest.java
@@ -2,14 +2,20 @@ package com.palmseung.keyword.service;
 
 import com.palmseung.keyword.domain.Keyword;
 import com.palmseung.keyword.domain.KeywordRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -34,5 +40,22 @@ public class KeywordServiceTest {
 
         //then
         verify(keywordRepository, times(1)).save(any(Keyword.class));
+    }
+
+    @DisplayName("키워드 - 이미 생성된 키워드")
+    @Test
+    void createKeywordWhenExisting(){
+        //given
+        Keyword keyword = Keyword.builder()
+                .id(1L)
+                .keyword("queendom")
+                .build();
+        given(keywordRepository.findByKeyword("queendom")).willReturn(Optional.of(keyword));
+
+        //when
+        keywordService.create(keyword);
+
+        //then
+        verify(keywordRepository, times(0)).save(any(Keyword.class));
     }
 }
