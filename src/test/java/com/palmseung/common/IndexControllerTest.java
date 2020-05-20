34deaf2b6104b910616ae3e9164087d46bcc5008
LeeSeungHee <palmseung@gmail.com>
2020-05-20 17:42:10 +0900

    Test (common) : add test for index page
    * Add IndexControllerTest.indexPage

---
+++ src/test/java/com/palmseung/common/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.palmseung.common;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IndexControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("index 페이지 출력")
+    @Test
+    void indexPage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
