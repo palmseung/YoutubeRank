9ce1bb4506b376706b66d6d7484ffbf1f93a7f96
LeeSeungHee <palmseung@gmail.com>
2020-05-20 17:48:53 +0900

    Feat (common) : add controller for index page
    * Add IndexController.indexPage

--- src/main/java/com/palmseung/common/IndexController.java
+++ src/main/java/com/palmseung/common/IndexController.java
@@ -1,7 +1,16 @@
 package com.palmseung.common;
 
+import com.palmseung.members.domain.Member;
+import com.palmseung.members.support.LoginUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    @RequestMapping("/")
+    public String indexPage(@LoginUser Member loginUser, Model model){
+        model.addAttribute("loginUser", loginUser);
+        return "index";
+    }
 }
