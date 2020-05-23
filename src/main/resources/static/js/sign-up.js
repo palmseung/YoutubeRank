9920dfc40a65e6af2cf7c4c32c460b453ba57eea
LeeSeungHee <palmseung@gmail.com>
2020-05-23 15:46:19 +0900

    Fix : modify front-end (3)

---
+++ src/main/resources/static/js/sign-up.js
@@ -0,0 +1,27 @@
+$('#signUpBtn').on('click', function (event) {
+    event.preventDefault();
+    alert('click btn');
+    create();
+});
+
+create = function () {
+    var data = {
+        email: $('#inputEmail').val(),
+        name: $('#inputName').val(),
+        password: $('#inputPassword').val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/members',
+        dataType: 'json',
+        async : false,
+        contentType: 'application/json',
+        data: JSON.stringify(data)
+    }).done(function(){
+        alert(data.name + '님 회원 가입을 환영합니다.');
+        window.location.href = '/';
+    }).fail(function(error){
+        alert(JSON.stringify(error));
+   });
+};
