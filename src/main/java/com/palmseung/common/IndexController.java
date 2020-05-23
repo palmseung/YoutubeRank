7028de2a82c89ede0c0695053ff65f3b888300bd
LeeSeungHee <palmseung@gmail.com>
2020-05-23 20:57:40 +0900

    Fix : fix test code (3)

--- src/main/java/com/palmseung/common/IndexController.java
+++ src/main/java/com/palmseung/common/IndexController.java
@@ -2,10 +2,8 @@ package com.palmseung.common;
 
 import com.palmseung.members.domain.Member;
 import com.palmseung.members.support.LoginUser;
-import com.palmseung.youtube.domain.YouTubeVideos;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,15 +14,4 @@ public class IndexController {
 
         return "index";
     }
-
-    @RequestMapping("/search-result")
-    public String searchResult(@LoginUser Member loginUser, Model model,
-                               @ModelAttribute("youTubeVideos") YouTubeVideos youTubeVideos) {
-        model.addAttribute("youTubeVideos", youTubeVideos.getYouTubeVideos());
-        model.addAttribute("loginUser", loginUser);
-
-//        return "layout/search-result";
-        return "layout/search-list";
-    }
-
 }
