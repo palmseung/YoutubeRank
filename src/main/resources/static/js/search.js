4ec67f2e2bdefee484d7a17677e467ac6cbb3ba2
LeeSeungHee <palmseung@gmail.com>
2020-05-25 16:08:56 +0900

    Fix : fix index page codes (4)

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -3,6 +3,10 @@ $('#searchBtn').on('click', function (event) {
     search();
 });
 
+$('#search-result-modal').on('hidden.bs.modal', function () {
+    location.reload();
+})
+
 function search(){
     var data = {
         keyword: $('#search').val()
@@ -49,6 +53,46 @@ function search(){
      });
 }
 
-$('#search-result-modal').on('hidden.bs.modal', function () {
-    location.reload();
-})
+function searchKeyword(keyword){
+    var data = {
+        keyword: encodeURIComponent(keyword)
+    };
+
+    $.ajax({
+          type: 'GET',
+          url: '/api/youtube?keyword='+data.keyword,
+          dataType: 'json',
+          async : false,
+      }).done(function(response){
+          alert('success');
+          $('#first_title').text(response[0].title);
+          $('#first_viewCount').text(response[0].viewCount);
+          $('#first_thumbnail').attr('src', response[0].thumbnailUrl);
+          $('#first_url').attr('href', response[0].url);
+
+          $('#second_title').text(response[1].title);
+          $('#second_viewCount').text(response[1].viewCount);
+          $('#second_thumbnail').attr('src', response[1].thumbnailUrl);
+          $('#second_url').attr('href', response[1].url);
+
+          $('#third_title').text(response[2].title);
+          $('#third_viewCount').text(response[2].viewCount);
+          $('#third_thumbnail').attr('src', response[2].thumbnailUrl);
+          $('#third_url').attr('href', response[2].url);
+
+          $('#fourth_title').text(response[3].title);
+          $('#fourth_viewCount').text(response[3].viewCount);
+          $('#fourth_thumbnail').attr('src', response[3].thumbnailUrl);
+          $('#fourth_url').attr('href', response[3].url);
+
+          $('#fifth_title').text(response[4].title);
+          $('#fifth_viewCount').text(response[4].viewCount);
+          $('#fifth_thumbnail').attr('src', response[4].thumbnailUrl);
+          $('#fifth_url').attr('href', response[4].url);
+
+          $('#keyword').text(keyword);
+          $('#search-result-modal').modal('show');
+      }).fail(function(error){
+          location.reload();
+     });
+}
