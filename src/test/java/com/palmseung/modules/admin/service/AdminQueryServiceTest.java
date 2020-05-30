4c7edffe9d4699682858f570de320d466366d185
LeeSeungHee <palmseung@gmail.com>
2020-05-31 02:01:38 +0900

    Test(admin) : add test to get all keywords
    * Add AdminQueryServiceTest.getAllKeywords

--- src/test/java/com/palmseung/modules/admin/service/AdminQueryServiceTest.java
+++ src/test/java/com/palmseung/modules/admin/service/AdminQueryServiceTest.java
@@ -2,6 +2,8 @@ package com.palmseung.modules.admin.service;
 
 import com.palmseung.BaseContainerTest;
 import com.palmseung.modules.admin.dto.AdminMemberResponseView;
+import com.palmseung.modules.keywords.dto.KeywordResponseView;
+import com.palmseung.modules.keywords.service.KeywordService;
 import com.palmseung.modules.members.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.palmseung.modules.keywords.KeywordConstant.TEST_KEYWORD;
+import static com.palmseung.modules.keywords.KeywordConstant.TEST_KEYWORD_2;
 import static com.palmseung.modules.members.MemberConstant.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -22,6 +26,9 @@ public class AdminQueryServiceTest extends BaseContainerTest {
 
     @MockBean
     private MemberService memberService;
+
+    @MockBean
+    private KeywordService keywordService;
 
     @DisplayName("어드민 계정 생성")
     @Test
@@ -49,5 +56,18 @@ public class AdminQueryServiceTest extends BaseContainerTest {
         //then
         assertThat(allMembers).hasSize(1);
         assertThat(allMembers.get(0).getEmail()).isEqualTo(TEST_EMAIL);
+    }
+
+    @DisplayName("등록된 전체 키워드 목록 조회")
+    @Test
+    void getAllKeywords() {
+        //given
+        given(keywordService.findAll()).willReturn(Arrays.asList(TEST_KEYWORD, TEST_KEYWORD_2));
+
+        //when
+        List<KeywordResponseView> allKeywords = adminQueryService.getAllKeywords();
+
+        //then
+        assertThat(allKeywords).hasSize(2);
     }
 }
